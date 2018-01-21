7c3c5d9a5bea5aca2a7b007b9df2781de9d07ff3
sameerahrshaik@gmail.com Shaik <sameerahrshaik@gmail.com>
2018-01-21 02:34:10 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,24 @@
 /*global describe, it*/
-
 describe('functions', () => {
   describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
+    it('returns "Happy holidays!"', ($name) => {
       expect(happyHolidays()).toEqual('Happy holidays!')
     })
   })
-
-  describe('happyHolidaysTo(name)', () => {
+}
+  describe('happyHolidaysTo(name)', ($name) => {
     it('returns "Happy holidays, ${name}!"', () => {
       expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
     })
   })
 
-  describe('happyHolidayTo(holiday, name)', () => {
+  describe('happyHolidayTo(holiday, name)', ($name) => {
     it('returns "Happy ${holiday}, ${name}!"', () => {
       expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
     })
   })
 
-  describe('holidayCountdown(holiday, days)', () => {
+  describe('holidayCountdown(holiday, days)', ($name) => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
       expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
     })
