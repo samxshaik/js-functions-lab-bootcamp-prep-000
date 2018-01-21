ebbef16e1706e2e584b629bc5283017ce2288e6b
sameerahrshaik@gmail.com Shaik <sameerahrshaik@gmail.com>
2018-01-21 04:57:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+var happyHolidays;
+var happyHolidayTo;
+
+function happyHolidays(){
+  console.log("Happy Holidays");
+}
+  function happyHolidayTo(holiday, name){
+   console.log( `Happy holidays, ${name}!`);
+    return happyHolidayTo();
+  }
+  function happyCountdown(days,holiday) {
+    console.log(`It's ${days} days until ${holiday}!`);
+    return happyCountdown();
+  }
+  return ;
